982e3461b61d47092f3994b5aaa52e6d001213fb
Javid-Shaik <shaikjavid8640@gmail.com>
2023-07-21 17:51:43 +0530

    dsa

---
+++ Dsa_By_Using_Java/ReomveDuplicates.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class ReomveDuplicates {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // creating a linkedlist and reading the data into it
+        LinkedList<Integer> list = new LinkedList<>();
+        System.out.print("Enter list1 size : ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter elements for list1 : ");
+        for(int i=0;i<n1;i++){
+            list.add(sc.nextInt());
+        }
+        // utility function for removing the duplicates
+        removeDuplicates(list);
+
+        // printing the elements after removing duplicates
+        for(Integer item : list){
+            System.out.print(item+" ");
+        }
+    }
+
+    private static void removeDuplicates(LinkedList<Integer>list){
+        // creating hashset for stroing unique elements
+        HashSet<Integer> seenElements = new HashSet<>();
+        LinkedList<Integer>unique = new LinkedList<>();
+        
+        // if the element is not already present in the set add else add it to duplicates list
+        for (Integer item : list) {
+            if (seenElements.add(item)) {
+                // This element is a duplicate
+                unique.add(item);
+            }
+        }
+        // clear the entire list and add the unique elements
+        list.clear();
+        list.addAll(unique);
+    }
+}
